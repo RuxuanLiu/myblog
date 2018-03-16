2d7da4518bbe22277ec02a586fd3e32e1cd99cb6
RuxuanLiu <1282906652@qq.com>
2018-03-16 19:03:49 +0800

    fix some problem

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<str:article_title>', views.detail, name='detail')
+    path('/<str:article_title>', views.detail, name='detail')
 ]
