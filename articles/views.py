a826f6c901c6f445662d929162f77e0d57bf03de
RuxuanLiu <1282906652@qq.com>
2018-03-17 16:39:25 +0800

    加上了简易评论功能

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, _get_queryset
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Article, Comment
 from markdown import markdown
+from django.urls import reverse
 # Create your views here.
 
 
@@ -10,14 +11,15 @@ def index(request):
 
 
 def detail(request, article_title):
-    article = get_object_or_404(Article, title =article_title)
+    article = get_object_or_404(Article, title=article_title)
     title = article.title
+    comment_list = Comment.objects.filter(article__title=article_title)
     content_render_by_markdown = markdown(article.content, extensions=[
                                      'markdown.extensions.extra',
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
-    context = {'title': title, 'content': content_render_by_markdown}
+    context = {'title': title, 'content': content_render_by_markdown, 'comment_list': comment_list}
     return render(request, 'articles/blog_main.html', context)
 
 
@@ -30,6 +32,7 @@ def home(request):
 def deal_comment(request, article_title):
     article = get_object_or_404(Article, title=article_title)
     comment_context = request.POST['comment']
-    comment = Comment.objects.create(article=article, context=comment_context, author='admin')
+    comment_author = request.POST['author']
+    comment = Comment.objects.create(article=article, context=comment_context, author=comment_author)
     comment.save()
-    return HttpResponse('评论完成')
+    return HttpResponseRedirect(reverse('articles:detail', args=(article_title,)))
