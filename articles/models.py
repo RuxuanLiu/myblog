4dbaaf9f579c0ecd372daf6a2e0dcde6bec7cc6e
RuxuanLiu <1282906652@qq.com>
2018-03-18 15:39:16 +0800

    增加了用户认证功能

--- articles/models.py
+++ articles/models.py
@@ -3,23 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Tag(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.content
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(null=True, blank=True)
     pub_date = models.DateTimeField('date published')
+    tag = models.ManyToManyField(Tag, null=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['-pub_date']
-
-
-class Tag(models.Model):
-    content = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.content
 
 
 class Comment(models.Model):
